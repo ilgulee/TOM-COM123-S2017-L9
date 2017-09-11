04819d599f37297e00760975f89510179c829291
ilgulee <iglee2000ca@gmail.com>
2017-09-10 20:43:43 -0400

    Added Human abstract class

---
+++ TOM-COM123-S2017-L9/Human.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TOM_COM123_S2017_L9
+{
+
+    abstract class Human
+    {
+        public string Name { get; set; }
+        public Human(string name)
+        {
+            this.Name = name;
+        }
+
+
+    }
+}
